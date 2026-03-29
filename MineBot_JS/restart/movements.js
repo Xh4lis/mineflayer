dcef1e5fed284e4c4a21cba73a68ae497f902b89
Thao Nguyen Quang <thao.nguyen-quang@insa-rennes.fr>
2026-03-29 19:01:43 +0200

    updated TODO list + watch.js and movements.js done

--- MineBot_JS/restart/movements.js
+++ MineBot_JS/restart/movements.js
@@ -1,3 +1,6 @@
+const casserBloc = 1;
+const deplacement = 1;
+const poserBloc = 1;
 class Movements {
     constructor(watch) {
         this.watch = watch;
@@ -7,12 +10,194 @@ class Movements {
     getNeighbors(node) {
         const neighbors = [];
         
-        // TODO: Vérifier les cases Nord, Sud, Est, Ouest
-        // Exemple Nord :
-        // Si this.watch.isWalkable(node.x, node.y, node.z - 1) est vrai,
-        // alors on l'ajoute à la liste 'neighbors' avec un coût (G) de 1.
+        // Nord
+        if (this.watch.isWalkable(node.x, node.y, node.z-1)) {
+            neighbors.push({x: node.x, y: node.y, z: node.z-1, cost: deplacement});
+            
+        }
+        else if (this.watch.isBreakable(node.x, node.y, node.z-1)) {
+            // On analyse ce qu'il faut casser exactement
+            const blocsACasser = [];
+            const pieds = this.watch.getBlock(node.x, node.y, node.z-1);
+            const tete = this.watch.getBlock(node.x, node.y+1, node.z-1);
+
+            if (pieds.name !== 'air') blocsACasser.push({ x: node.x, y: node.y, z: node.z-1 });
+            if (tete.name !== 'air') blocsACasser.push({ x: node.x, y: node.y+1, z: node.z-1 });
+
+            // Le coût dépend du nombre de blocs à casser ! (Casser 2 blocs prend 2x plus de temps)
+            const coutTotal = deplacement + (blocsACasser.length*casserBloc); // 1 pour avancer + 5 par bloc à casser
+
+            neighbors.push({ 
+                x: node.x, y: node.y, z: node.z-1, 
+                cost: coutTotal, 
+                action: 'break', 
+                toBreak: blocsACasser // On donne la liste des blocs à la pioche !
+            });
+        }
+        else {
+            // Descente
+            const videPieds = this.watch.getBlock(node.x, node.y, node.z - 1);
+            const videTete = this.watch.getBlock(node.x, node.y + 1, node.z - 1);
+
+            if (videPieds && videTete && videPieds.boundingBox === 'empty' && videTete.boundingBox === 'empty') {
+                if (this.watch.isWalkable(node.x, node.y - 1, node.z - 1)) { // La case de réception en bas est bonne
+                    neighbors.push({ 
+                        x: node.x, y: node.y - 1, z: node.z - 1, 
+                        cost: deplacement, // Tomber ne coûte pas vraiment d'énergie
+                        action: 'drop' 
+                    });
+                }
+            }
+        }
+
+        // Sud
+        if (this.watch.isWalkable(node.x, node.y, node.z+1)) {
+            neighbors.push({x: node.x, y: node.y, z: node.z+1, cost: deplacement});
+        }
+        else if (this.watch.isBreakable(node.x, node.y, node.z+1)) {
+            const blocsACasser = [];
+            const pieds = this.watch.getBlock(node.x, node.y, node.z+1);
+            const tete = this.watch.getBlock(node.x, node.y+1, node.z+1);
+            if (pieds.name !== 'air') blocsACasser.push({ x: node.x, y: node.y, z: node.z+1 });
+            if (tete.name !== 'air') blocsACasser.push({ x: node.x, y: node.y+1, z: node.z+1 });
+            const coutTotal = deplacement + (blocsACasser.length*casserBloc);
+            neighbors.push({ 
+                x: node.x, y: node.y, z: node.z+1, 
+                cost: coutTotal, 
+                action: 'break', 
+                toBreak: blocsACasser 
+            });
+        }
+        else {
+            // Descente
+            const videPieds = this.watch.getBlock(node.x, node.y, node.z + 1);
+            const videTete = this.watch.getBlock(node.x, node.y + 1, node.z + 1);
+
+            if (videPieds && videTete && videPieds.boundingBox === 'empty' && videTete.boundingBox === 'empty') {
+                if (this.watch.isWalkable(node.x, node.y - 1, node.z + 1)) { // La case de réception en bas est bonne
+                    neighbors.push({ 
+                        x: node.x, y: node.y - 1, z: node.z + 1, 
+                        cost: deplacement, // Tomber ne coûte pas vraiment d'énergie
+                        action: 'drop' 
+                    });
+                }
+            } 
+        }
+        // Est
+        if (this.watch.isWalkable(node.x+1, node.y, node.z)) {
+            neighbors.push({x: node.x+1, y: node.y, z: node.z, cost: deplacement});
+        }
+        else if (this.watch.isBreakable(node.x+1, node.y, node.z)) {
+            const blocsACasser = [];
+            const pieds = this.watch.getBlock(node.x+1, node.y, node.z);
+            const tete = this.watch.getBlock(node.x+1, node.y+1, node.z);
+            if (pieds.name !== 'air') blocsACasser.push({ x: node.x+1, y: node.y, z: node.z });
+            if (tete.name !== 'air') blocsACasser.push({ x: node.x+1, y: node.y+1, z: node.z });
+            const coutTotal = deplacement + (blocsACasser.length*casserBloc);
+            neighbors.push({ 
+                x: node.x+1, y: node.y, z: node.z, 
+                cost: coutTotal, 
+                action: 'break', 
+                toBreak: blocsACasser 
+            });
+        }
+        else {
+            // Descente
+            const videPieds = this.watch.getBlock(node.x+1, node.y, node.z);
+            const videTete = this.watch.getBlock(node.x+1, node.y + 1, node.z);
+
+            if (videPieds && videTete && videPieds.boundingBox === 'empty' && videTete.boundingBox === 'empty') {
+                if (this.watch.isWalkable(node.x+1, node.y - 1, node.z)) { // La case de réception en bas est bonne
+                    neighbors.push({ 
+                        x: node.x+1, y: node.y - 1, z: node.z, 
+                        cost: deplacement, // Tomber ne coûte pas vraiment d'énergie
+                        action: 'drop' 
+                    });
+                }
+            }
+        }
+        // Ouest
+        if (this.watch.isWalkable(node.x-1, node.y, node.z)) {
+            neighbors.push({x: node.x-1, y: node.y, z: node.z, cost: deplacement});
+        }
+        else if (this.watch.isBreakable(node.x-1, node.y, node.z)) {
+            const blocsACasser = [];
+            const pieds = this.watch.getBlock(node.x-1, node.y, node.z);
+            const tete = this.watch.getBlock(node.x-1, node.y+1, node.z);
+            if (pieds.name !== 'air') blocsACasser.push({ x: node.x-1, y: node.y, z: node.z });
+            if (tete.name !== 'air') blocsACasser.push({ x: node.x-1, y: node.y+1, z: node.z });
+            const coutTotal = deplacement + (blocsACasser.length*casserBloc);
+            neighbors.push({ 
+                x: node.x-1, y: node.y, z: node.z, 
+                cost: coutTotal, 
+                action: 'break', 
+                toBreak: blocsACasser 
+            });
+        }
+        else {
+            // Descente
+            const videPieds = this.watch.getBlock(node.x-1, node.y, node.z);
+            const videTete = this.watch.getBlock(node.x-1, node.y + 1, node.z);
+            if (videPieds && videTete && videPieds.boundingBox === 'empty' && videTete.boundingBox === 'empty') {
+                if (this.watch.isWalkable(node.x-1, node.y - 1, node.z)) { // La case de réception en bas est bonne
+                    neighbors.push({ 
+                        x: node.x-1, y: node.y - 1, z: node.z, 
+                        cost: deplacement, // Tomber ne coûte pas vraiment d'énergie
+                        action: 'drop' 
+                    });
+                }
+            }
+        }
         
-        // TODO (optionnel) : Ajouter la logique pour les sauts et les chutes
+        // (optionnel) : Ajouter la logique pour les sauts et les chutes
+
+        if (this.watch.canTowerUp(node.x, node.y, node.z)) {
+            // Coût modéré : sauter et poser un bloc ça demande un petit effort
+            neighbors.push({ x: node.x, y: node.y + 1, z: node.z, cost: deplacement + poserBloc, action: 'tower' });
+            //Nord
+            if (this.watch.isWalkable(node.x, node.y + 1, node.z - 1)) { // La case d'arrivée en hauteur est libre
+                neighbors.push({ 
+                    x: node.x, y: node.y + 1, z: node.z - 1, 
+                    cost: deplacement + 0.5, // Un saut coûte un peu plus d'énergie que marcher
+                    action: 'jump' 
+                });
+            }
+            //Sud
+            if (this.watch.isWalkable(node.x, node.y + 1, node.z + 1)) { // La case d'arrivée en hauteur est libre
+                neighbors.push({ 
+                    x: node.x, y: node.y + 1, z: node.z + 1, 
+                    cost: deplacement + 0.5, // Un saut coûte un peu plus d'énergie que marcher
+                    action: 'jump' 
+                });
+            }
+            //Est
+            if (this.watch.isWalkable(node.x + 1, node.y + 1, node.z)) { // La case d'arrivée en hauteur est libre
+                neighbors.push({ 
+                    x: node.x + 1, y: node.y + 1, z: node.z, 
+                    cost: deplacement + 0.5, // Un saut coûte un peu plus d'énergie que marcher
+                    action: 'jump' 
+                });
+            }
+            //Ouest
+            if (this.watch.isWalkable(node.x - 1, node.y + 1, node.z)) { // La case d'arrivée en hauteur est libre
+                neighbors.push({ 
+                    x: node.x - 1, y: node.y + 1, z: node.z, 
+                    cost: deplacement + 0.5, // Un saut coûte un peu plus d'énergie que marcher
+                    action: 'jump' 
+                });
+            }
+        } else if (this.watch.canBreakAndTowerUp(node.x, node.y, node.z)) {
+            // Coût élevé : il faut miner, puis sauter, puis poser le bloc
+            neighbors.push({ 
+                x: node.x, y: node.y + 1, z: node.z, 
+                cost: deplacement + casserBloc + poserBloc, // 1 pour casser + 2 pour sauter/poser + 1 de pénalité pour la complexité 
+                action: 'break_and_tower',
+                toBreak: [{ x: node.x, y: node.y + 2, z: node.z }] // On dit au contrôleur de casser le plafond !
+            });
+        }
+
+
+
 
         return neighbors; 
     }
--- MineBot_JS/restart/watch.js
+++ MineBot_JS/restart/watch.js
@@ -4,16 +4,88 @@ class Watch {
     }
 
     getBlock(x, y, z) {
-        // TODO: utiliser this.bot.blockAt(pos) pour retourner le bloc
+        return this.bot.blockAt({x, y, z});
     }
 
     // Détermine si le bot peut se tenir sur cette case précise
     isWalkable(x, y, z) {
-        // TODO: 
-        // 1. Lire le bloc en (x, y, z) (doit être de l'air/traversable)
-        // 2. Lire le bloc au-dessus (x, y+1, z) (doit être de l'air)
-        // 3. Lire le bloc en-dessous (x, y-1, z) (doit être solide)
-        // Retourner true si c'est bon, false sinon.
+        if (y < -64||y>319) return false; // limites du monde
+        const sol = this.getBlock(x, y-1, z);
+        const pieds = this.getBlock(x, y, z);
+        const tete = this.getBlock(x, y+1, z);
+        if (!sol || !pieds || !tete) return false;
+        if (sol.boundingBox === 'block') { // le bloc en dessous doit être solide
+            if (pieds.boundingBox === 'empty' && tete.boundingBox === 'empty') { // les blocs à la hauteur du bot doivent être de l'air
+                return true;
+            }
+        }
+        return false;
+    }
+    // Détermine si le bot peut casser le bloc à côté de lui (dans la direction donnée) et le bloc en dessous pour s'y frayer un chemin (checker si le bloc encore au dessus n'est pas de la lave/eau/gravier/sable/enclume ? | et que le bloc encore en dessous est solide)
+    isBreakable(x, y, z) {
+        if (y < -64||y>319) return false; // limites du monde
+        const sol = this.getBlock(x, y-1, z);
+        const pieds = this.getBlock(x, y, z);
+        const tete = this.getBlock(x, y+1, z);
+        const plafond = this.getBlock(x, y+2, z);
+        if (sol.boundingBox === 'block') { // le bloc en dessous doit être solide
+            if (!["sand","anvil","gravel"].includes(plafond.name)) { // s'il y a un bloc au dessus, il ne doit pas être du sable/gravier/enclume/lave/eau
+                if ((pieds.diggable === true||pieds.boundingBox === 'empty') && (tete.diggable === true||pieds.boundingBox === 'empty') ) { // les blocs à la hauteur du bot doivent être de l'air
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Détermine si on peut creuser le bloc exactement sous nos pieds
+    canDigDown(x, y, z) {
+        if (y < -63) return false; // On ne creuse pas la Bedrock
+
+        const blocAScasser = this.getBlock(x, y - 1, z);
+        const blocDeReception = this.getBlock(x, y - 2, z); // Le bloc sur lequel on va atterrir
+
+        if (!blocAScasser || !blocDeReception) return false;
+
+        // Le bloc sous nos pieds doit être cassable
+        if (blocAScasser.diggable === true) {
+            const dangers = ['lava', 'water'];
+            if (!dangers.includes(blocDeReception.name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // Détermine si le bot a la place de sauter pour poser un bloc sous lui
+    canTowerUp(x, y, z) {
+        if (y > 319) return false;
+
+        const espaceTeteSaut = this.getBlock(x, y + 2, z);
+
+        if (!espaceTeteSaut) return false;
+
+        // Pour sauter sur place, il suffit que le bloc au-dessus de notre tête soit vide
+        return espaceTeteSaut.boundingBox === 'empty';
+    }
+
+    // Détermine si on peut casser le plafond pour ensuite faire un Tower Up
+    canBreakAndTowerUp(x, y, z) {
+        if (y > 318) return false;
+
+        const plafond = this.getBlock(x, y + 2, z);
+        const auDessusDuPlafond = this.getBlock(x, y + 3, z);
+
+        if (!plafond || !auDessusDuPlafond) return false;
+
+        // Le plafond doit être un bloc solide, mais cassable
+        if (plafond.boundingBox === 'block' && plafond.diggable === true) {
+            
+            const dangers = ['sand', 'gravel', 'anvil', 'lava', 'water'];
+            if (!dangers.includes(auDessusDuPlafond.name)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
