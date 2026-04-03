6f7c1a82febda0b0fab7a9c47b7461bbe5e99f77
Thao Nguyen Quang <thao.nguyen-quang@insa-rennes.fr>
2026-04-03 15:39:44 +0200

    feature: controller.js true/false state per method

--- MineBot_JS/restart/controller.js
+++ MineBot_JS/restart/controller.js
@@ -20,23 +20,27 @@ class Controller {
 
             // on vise les pieds du bloc cible + offset pour centrer
             await this.bot.lookAt(cible.offset(0.5, 0, 0.5));
-
+            let succes = false;
             // on exécute l'action demandée
             if (etape.action === 'walk' || etape.action === 'drop') {
-                await this.marcherVers(cible);
+                succes = await this.marcherVers(cible);
             } 
             else if (etape.action === 'jump') {
-                await this.sauterVers(cible);
+                succes = await this.sauterVers(cible);
             }
             else if (etape.action === 'break') {
-                await this.casserEtMarcherVers(cible, etape.toBreak);
+                succes = await this.casserEtMarcherVers(cible, etape.toBreak);
             }
             else if (etape.action === 'tower') {
-                await this.tower(cible);
+                succes = await this.tower(cible);
             }
             else if (etape.action === 'break_and_tower') {
-                await this.breakTower(cible, etape.toBreak);
+                succes = await this.breakTower(cible, etape.toBreak);
             }
+            if (!succes) {
+                console.log("Je suis bloqué ! J'abandonne ce chemin.");
+                break; // On casse la boucle for de l'executePath
+            }   
             
             // On fait une micro-pause pour éviter que le bot ne glisse ou n'aille trop vite
             await this.wait(50); 
@@ -50,6 +54,7 @@ class Controller {
     async marcherVers(cible) {
         this.bot.setControlState('forward', true);
 
+        let tempsEcoule = 0; // chronomètre
         // on crée une boucle qui attend que le bot soit arrivé
         while (true) {
             // distance au centre du bloc visé (X et Z, on ignore un peu la hauteur précise)
@@ -58,9 +63,15 @@ class Controller {
 
             if (distance < 0.3) {
                 this.bot.setControlState('forward', false);
-                break;
+                return true;
+            }
+            if (tempsEcoule > 2000) {
+                this.bot.setControlState('forward', false);
+                this.bot.clearControlStates(); // On lâche tout
+                return false; // Échec du mouvement
             }
             await this.wait(50); // On attend 50ms avant de revérifier la distance
+            tempsEcoule += 50; // On ajoute 50ms au chronomètre
         }
     }
 
@@ -70,35 +81,41 @@ class Controller {
         await this.wait(100);
         this.bot.setControlState('jump', false);
 
+        let tempsEcoule = 0; // chronomètre
         while (true) {
             const botPos = this.bot.entity.position;
             const distance = Math.sqrt(Math.pow(botPos.x - (cible.x + 0.5), 2) + Math.pow(botPos.z - (cible.z + 0.5), 2));
 
             if (distance < 0.3 && Math.abs(botPos.y - cible.y) < 0.1) {
                 this.bot.setControlState('forward', false);
-                break;
+                return true;
             }
-            await this.wait(50);
+            if (tempsEcoule > 2000) {
+                this.bot.setControlState('forward', false);
+                this.bot.clearControlStates(); // On lâche tout
+                return false; // Échec du mouvement
+            }
+            await this.wait(50); // On attend 50ms avant de revérifier la distance
+            tempsEcoule += 50; // On ajoute 50ms au chronomètre
         }
     }
     async breakBlocks(blocsACasser) {
+        //prendre le bon outil si est dans l'inventaire
         this.equiperMeilleurOutil(this.bot.blockAt(new Vec3(blocsACasser[0].x, blocsACasser[0].y, blocsACasser[0].z))); // On équipe le meilleur outil pour le premier bloc à casser (s'il y en a un)
         for (const bloc of blocsACasser) {
             const blockToDig = this.bot.blockAt(new Vec3(bloc.x, bloc.y, bloc.z));
             await this.bot.lookAt(blockToDig.position.offset(0.5, 0, 0.5));
-            //TODO : rajouter prendre le bon outil si est dans l'inventaire
             await this.bot.dig(blockToDig);
         }
     }
     async casserEtMarcherVers(cible, blocsACasser) {
         await this.breakBlocks(blocsACasser);
         await this.bot.lookAt(cible.offset(0.5, 0, 0.5));
-        await this.marcherVers(cible);
+        return await this.marcherVers(cible);
     }
     // TODO : (optionnel) faire une fonction qui place les blocs mais il faudra enlever dans les autres fonctions qui le font
 
     async tower(cible) {
-        // TODO : S'assurer que le bot a équipé un bloc (dirt, cobble...) dans sa main !
         const positionActuelle = this.bot.entity.position;
         const blocSousPieds = this.bot.blockAt(positionActuelle.offset(0, -1, 0));
         // Le vecteur qui indique le "dessus" du bloc
@@ -113,20 +130,23 @@ class Controller {
             await this.bot.placeBlock(blocSousPieds, faceHaut);
         } catch (err) {
             // Si on n'a pas de blocs en main ou qu'on a raté le timing, ça va déclencher cette erreur
-            console.log("Impossible de faire le tower (pas de bloc en main ?) :", err.message);
+            this.bot.setControlState('jump', false);
+            console.log("Impossible de faire le tower :", err.message);
+            return false;
         }
         this.bot.setControlState('jump', false);
         await this.wait(150);
+        return true;
     }
 
     async breakTower(cible, blocsACasser) {
         await this.breakBlocks(blocsACasser);
-        await this.tower(cible);
+        return await this.tower(cible);
     }
     async digDown(cible, blocSousPieds) {
         await this.breakBlocks([{ x: blocSousPieds.x, y: blocSousPieds.y, z: blocSousPieds.z }]);
         await this.bot.lookAt(cible.offset(0.5, 0, 0.5));
-        await this.marcherVers(cible);
+        return await this.marcherVers(cible);
     }
 
     async equiperBloc() {
